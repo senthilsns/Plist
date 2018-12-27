c33de2ebe500e2f613ef3111f39a6b3ca230ce89
Senthil kumar K <nightrider.cbe@gmail.com>
2018-12-27 23:31:51 +0530

    Update

--- Plist/ViewController.swift
+++ Plist/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var nameField: UITextField!
+    
+    @IBOutlet var positionField: UITextField!
+    
+    @IBAction func addAction(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
